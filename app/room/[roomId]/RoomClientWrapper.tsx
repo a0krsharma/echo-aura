71bc133ec89a325a24d867bef35b31f9aa5cb319
Echo Aura <echo@aura.app>
2026-08-12 04:21:30 +0530

    Remove dynamic import to fix caching issues

--- app/room/[roomId]/RoomClientWrapper.tsx
+++ app/room/[roomId]/RoomClientWrapper.tsx
@@ -1,21 +1,7 @@
 "use client";
 
-import dynamic from "next/dynamic";
 import { useParams } from "next/navigation";
-
-const RoomClient = dynamic(
-  () => import("./RoomClient"),
-  {
-    ssr: false,
-    loading: () => (
-      <div className="min-h-screen bg-black text-white flex flex-col items-center justify-center font-mono text-xs tracking-[0.2em] uppercase">
-        <div className="border border-neutral-800 p-6 animate-pulse">
-          [ CONNECTING TO ROOM... ]
-        </div>
-      </div>
-    ),
-  }
-);
+import RoomClient from "./RoomClient";
 
 export default function RoomClientWrapper() {
   const params = useParams();
